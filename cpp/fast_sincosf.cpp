b84d79506755505a8f31c6bae7ca5a967126202d
Perttu Yli-Opas <perttu.yli-opas@aalto.fi>
2018-09-19 14:35:19 +0300

    Modified the FastSin to use vector instructions to shave off another clock cycle

--- cpp/fast_sincosf.cpp
+++ cpp/fast_sincosf.cpp
@@ -8,6 +8,9 @@
 
 using namespace std;
 
+typedef double double2_t __attribute__ ((vector_size (2 * sizeof(double))));
+double2_t double2_0 = {0, 0};
+
    // lookup table contains 64 values for sin(x), where 0 <= x < 2*pi
 double lookup[PRECISION];
 
@@ -63,20 +66,25 @@ double FastSin(double x)
 	int index2 = (index + (PRECISION/4)) & (PRECISION-1);
 
 	// s is the closest value of sine from lookup table, c is cos
-	double s = lookup[index1];
-	double c = lookup[index2];
+	double2_t sc = {lookup[index1], lookup[index2]};
 
 	double dt2 = dt * dt;
 	double dt4 = dt2 * dt2;
 
-	double second = 1 - dt2 * (1./6.);
-	double first = 1 - dt2 * (1./2.);
+	double2_t dt2_ = {dt2, dt2};
+	double2_t dt4_ = {dt4, dt4};
+	
+	double2_t parts = {1, 1};
 
-	second += dt4 * (1./120.);
-	first += dt4 * (1./24.);
+	double2_t mult1 = {1./2., 1./6.};
+	parts -= dt2_ * mult1;
 
-	second *= dt;
-	return s * first + c * second;
+	double2_t mult2 = {1./24., 1./120.};
+	parts += dt4_ * mult2;
+
+	parts *= sc;
+
+	return parts[0] + parts[1] * dt;
 
 }
 
